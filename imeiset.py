2268cd0565bff46d41c9708102dddaad67f96e66
u0d7i <u0d7i@users.noreply.github.com>
2020-06-14 12:31:35 +0300

    do_tac

--- imeiset.py
+++ imeiset.py
@@ -25,12 +25,25 @@ def do_random():
     IMEI = IMEI0 + CD
     print(IMEI,'- random')
 
+def do_tac():
+    check_tacfile()
+    tac_list=read_tacfile()
+    TAC=tac_list[0]
+    SN = f'{randrange(999999):06}'
+    IMEI0 = TAC + SN
+    CD = luhn.calc_check_digit(IMEI0)
+    IMEI = IMEI0 + CD
+    print(IMEI,'-',tac_list[1],tac_list[2])
+
+def do_factory():
+    print('Not implemented yet')
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Set IMEI (education purposes only)')
     parser.add_argument('-s','--set',action='store_true', help='Actually set (default is print)')
     parser.add_argument('-q','--quiet',action='store_true', help='Be quiet')
     parser.add_argument('-f','--file',action='store', help='TAC file')
+    parser.add_argument('-d','--dev',action='store', help='Modem device')
     gr_req = parser.add_mutually_exclusive_group(required=True)
     gr_req.add_argument('-r','--random',action='store_true', help="Random IMEI")
     gr_req.add_argument('-t','--tac',action='store_true', help="Random real TAC IMEI")
@@ -52,7 +65,6 @@ def read_tacfile():
         tac_list=csv_data[randrange(2,row_count)]
     return tac_list
 
-
 args=parse_args()
 # remove this after cleanup
 # print(args)
@@ -66,13 +78,5 @@ if args.update:
 if args.random:
     do_random()
 
-
 if args.tac:
-    check_tacfile()
-    tac_list=read_tacfile()
-    TAC=tac_list[0]
-    SN = f'{randrange(999999):06}'
-    IMEI0 = TAC + SN
-    CD = luhn.calc_check_digit(IMEI0)
-    IMEI = IMEI0 + CD
-    print(IMEI,'-',tac_list[1],tac_list[2])
+    do_tac()
