7b01266cb79cca64be22ac4e6fd22ab44505854b
u0d7i <u0d7i@users.noreply.github.com>
2020-06-14 11:15:00 +0300

    tmp

--- imeiset.py
+++ imeiset.py
@@ -19,9 +19,17 @@ def do_update():
     import urllib.request
     urllib.request.urlretrieve (tac_url, tac_file)
 
+def do_random():
+    IMEI0 = f'{randrange(99999999999999):014}'
+    CD = luhn.calc_check_digit(IMEI0)
+    IMEI = IMEI0 + CD
+    print(IMEI,'- random')
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Set IMEI (education purposes only)')
     parser.add_argument('-s','--set',action='store_true', help='Actually set (default is print)')
+    parser.add_argument('-q','--quiet',action='store_true', help='Be quiet')
     parser.add_argument('-f','--file',action='store', help='TAC file')
     gr_req = parser.add_mutually_exclusive_group(required=True)
     gr_req.add_argument('-r','--random',action='store_true', help="Random IMEI")
@@ -34,10 +42,20 @@ def parse_args():
     if not vars(args):
         parser.print_help()
         parser.exit(1)
+    return args
 
-parse_args()
+def read_tacfile():
+    with open(tac_file,'r') as file:
+        csv_reader = reader(file, delimiter=',')
+        csv_data = list(csv_reader)
+        row_count = len(csv_data)
+        tac_list=csv_data[randrange(2,row_count)]
+    return tac_list
+
+
+args=parse_args()
 # remove this after cleanup
-print(args)
+# print(args)
 
 if args.file:
     tac_file=args.file
@@ -45,22 +63,16 @@ if args.file:
 if args.update:
     do_update()
 
+if args.random:
+    do_random()
+
+
 if args.tac:
     check_tacfile()
-    print('tacfile exists')
-
-# cleanup below this line
-quit()
-
-with open(tac_file,'r') as file:
-    csv_reader = reader(file, delimiter=',')
-    csv_data = list(csv_reader)
-    row_count = len(csv_data)
-    tac_list=csv_data[randrange(2,row_count)]
-
-TAC=tac_list[0]
-SN = f'{randrange(999999):06}'
-IMEI0 = TAC + SN
-CD = luhn.calc_check_digit(IMEI0)
-IMEI = IMEI0 + CD
-print(IMEI,'-',tac_list[1],tac_list[2])
+    tac_list=read_tacfile()
+    TAC=tac_list[0]
+    SN = f'{randrange(999999):06}'
+    IMEI0 = TAC + SN
+    CD = luhn.calc_check_digit(IMEI0)
+    IMEI = IMEI0 + CD
+    print(IMEI,'-',tac_list[1],tac_list[2])
